57dc3f31bf9a2acc86476ac1f953be6ed3092c49
Jacky Chung <clashofjacky@gmail.com>
2023-11-04 14:59:59 +0800

    add log out function

--- devhacktesting/lib/screens/Personal/Personal_page.dart
+++ devhacktesting/lib/screens/Personal/Personal_page.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import 'package:google_nav_bar/google_nav_bar.dart';
 
 class Personal extends StatelessWidget {
+  const Personal({super.key});
+
+  // temp function for sign out user
+  void signOutUser() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(actions: [
+        IconButton(
+          onPressed: signOutUser,
+          icon: Icon(Icons.logout),
+        )
+      ]),
       body: Center(
         child: Text('personal'),
       ),
